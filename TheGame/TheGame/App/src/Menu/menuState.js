8e09365689f5863eaced853932d86ef28eeefe23
Marck Jensen <marckjensen93@hotmail.com>
2016-05-10 21:34:19 +0200

    Fixed menu buttons

--- TheGame/TheGame/App/src/Menu/menuState.js
+++ TheGame/TheGame/App/src/Menu/menuState.js
@@ -16,16 +16,16 @@ menu.prototype = {
         this.game.add.tileSprite(0, 0, 1080, 720, 'MenuBackground');
 
         //Setting up the text
-        var title = this.game.add.text(400, 150, 'Epoch', {
+        var title = this.game.add.text(540, 150, 'Epoch', {
             font: 'Bold 64px Arial',
             fill: '#660000'
         });
         title.anchor.set(0.5);
 
         //Setup all the buttons
-        startBtn = this.game.add.button(400, 300, 'MenuStartButton', this.startGame, this);
-        continueBtn = this.game.add.button(400, 350, 'MenuContinueButton', this.continueGame, this);
-        quitBtn = this.game.add.button(400, 400, 'MenuQuitButton', this.quitGame, this);
+        startBtn = this.game.add.button(540, 300, 'MenuStartButton', this.startGame, this);
+        continueBtn = this.game.add.button(540, 350, 'MenuContinueButton', this.continueGame, this);
+        quitBtn = this.game.add.button(540, 400, 'MenuQuitButton', this.quitGame, this);
         startBtn.anchor.set(0.5);
         continueBtn.anchor.set(0.5);
         quitBtn.anchor.set(0.5);
