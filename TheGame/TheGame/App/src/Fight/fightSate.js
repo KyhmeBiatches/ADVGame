29203d43208113f5e84bf5bfd08a94c36be26aef
Nicolai Reigstad <nicolai887@gmail.com>
2016-05-23 01:26:31 +0200

    Added stuff :D

--- TheGame/TheGame/App/src/Fight/fightSate.js
+++ TheGame/TheGame/App/src/Fight/fightSate.js
@@ -13,13 +13,13 @@ fight.prototype = {
         this.load.image('arena-' + number, 'App/assets/fighting/arenas/arena' + number + '.png');
         //load enemy
         this.game.load.spritesheet('enemy' + number, 'App/assets/fighting/enemy/enemy-' + number + '.png', 480, 550);
-        this.load.image('enemy' + number + 'bullet', 'App/assets/fighting/enemy/enemy-' + number + 'projectile.png')
+        this.load.image('enemy' + number + 'bullet', 'App/assets/fighting/enemy/enemy-' + number + '-projectile.png')
         
     },
     create: function() {
         //Setup background image
         var background = this.game.add.tileSprite(0, 0, 1080, 720, 'arena-' + number);
-        background.sacle.set(0);
+        //background.sacle.set(0);
     },
     update: function() {
         
