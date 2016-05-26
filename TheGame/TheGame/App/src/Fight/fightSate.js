9f89f1a32461069fb92eee767cae652361a880b5
Nicolai Reigstad <nicolai887@gmail.com>
2016-05-26 04:33:02 +0200

    Sounds and backmoon walk

--- TheGame/TheGame/App/src/Fight/fightSate.js
+++ TheGame/TheGame/App/src/Fight/fightSate.js
@@ -78,7 +78,7 @@ fight.prototype = {
 
         //Create player animations 
         player.animations.add(walkRightAnimation, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
-        player.animations.add(walkLeftAnimation, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+        player.animations.add(walkLeftAnimation, [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
         player.animations.add('none', [8]);
         player.animations.play('none', 1, true);
 
